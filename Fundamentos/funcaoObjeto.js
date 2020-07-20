41f36a531bc6fe923fb9faa2a37200b0020793e4
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-07-20 15:06:38 -0300

    FuncaoVsObjeto

---
+++ Fundamentos/funcaoObjeto.js
@@ -0,0 +1,13 @@
+console.log(typeof Object)
+console.log(typeof new Object())
+
+const Cliente = function() {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente())
+
+class Produto {} //ES 2015 (ES6)
+console.log(typeof Produto)
+console.log(typeof new Produto())
+
+//Em java script tudo e tratado como funcão até ser instanciado
+//A forma de criar um objeto em java script e a partir de uma função
