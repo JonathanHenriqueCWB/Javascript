4a7838e3a5f5f89320bbbe6f9b793602346c790e
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-07-30 17:42:35 -0300

    Funcao - tipo de declaracao

---
+++ Funcoes/tiposDeclaracao.js
@@ -0,0 +1,22 @@
+// Declaraçções de funcções javascript
+
+// function declaration
+function soma(x, y){
+    return x + y
+}
+
+// Função expression
+const sub = function(x, y){
+    return x - y 
+}
+
+// Named function expression
+const mult = function mult(x, y){
+    return x * y
+}
+
+/* 
+    Caso uma função seja armazenada em uma variavel ou constante, a mesmo não poderá
+    ser chamada antes da função, dierente da function declaration que pode ser chamada
+    antes
+ */
