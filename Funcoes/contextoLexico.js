034b5eaab9881ce0bdb73bb1c8320b5cdc6c32f0
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-07-31 15:52:57 -0300

    Funcao - contexto lexico e closures

---
+++ Funcoes/contextoLexico.js
@@ -0,0 +1,13 @@
+// Exemplo de contexto lexico
+const valor = 'Global'
+
+function minhaFuncao(){
+    console.log(valor)
+}
+
+function exec(){
+    const valor = 'Local'
+    minhaFuncao()
+}
+
+exec()
