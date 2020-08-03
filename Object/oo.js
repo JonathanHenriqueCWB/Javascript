2f7795b476ce3d118cac4c79a0a5ecc531f643b3
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-08-03 10:44:06 -0300

    Obejto - pilares

---
+++ Object/oo.js
@@ -0,0 +1,15 @@
+// Não executável
+
+// Precedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...  
+    }
+}
+objeto.processamento()
