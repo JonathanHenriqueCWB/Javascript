ada2b35f7ae9c32c18a9db7647e1d2adc0e73ddc
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-08-17 20:16:44 -0300

    Array - filter intro

---
+++ Arrays/Filter/filter1.js
@@ -0,0 +1,32 @@
+// Filter com array
+const frutas = ['Banana', 'Maça', 'Uva', 'Tomate', 'Abacaxi']
+const frutasFiltrada = frutas.filter(function(e){
+    return e != 'Tomate'
+})
+console.log('Frutas: ', frutasFiltrada)
+
+// Filter com array de objetos
+const produtos = [
+    {nome: 'Notebook', preco: 2999, fragil: true},
+    {nome: 'Mouse', preco: 59.99, fragil: true},
+    {nome: 'GPU 570', preco: 999.99, fragil: true},
+    {nome: 'Teclado', preco: 199.90, fragil: false}
+]
+
+const fragil = e => e.fragil == true
+const caro = e => e.preco > 900
+
+const produtosFiltrados = produtos.filter(fragil).filter(caro)
+
+/*
+const produtosFiltrados = produtos.filter(function(p){
+    return p.fragil === true && p.preco > 900
+})
+*/
+console.log('Produtos: ', produtosFiltrados)
+
+/**
+ * Por padrão filter recebe tres parâmetros, o item atual, indice e o próprio array
+ * Ambas propriedades podem ser utilizadas no filtro 
+ */
+
