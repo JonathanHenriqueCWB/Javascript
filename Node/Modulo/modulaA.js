07842f9c201646933cd032cff4a4def883ed5d2b
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-08-21 18:24:05 -0300

    Node - sistema de modulos

---
+++ Node/Modulo/modulaA.js
@@ -0,0 +1,4 @@
+// Exportando modulos
+this.ola = 'Ola'
+exports.bemVidndo = 'Bem vindo'
+module.exports.ateLogo = 'Até logo!'
---
+++ Node/Modulo/moduloCliente.js
@@ -0,0 +1,13 @@
+// Importando modulos externos
+const moduloA = require('./modulaA')
+const moduloB = require('./moduloB')
+
+// Usando o modula A
+console.log('Modula A:', moduloA.ola)
+console.log('Modula A:', moduloA.bemVidndo)
+console.log('Modula A:', moduloA.ateLogo)
+
+// Usando modulo B
+console.log('Modulo B', moduloB.bomDia)
+console.log('Modulo B', moduloB.boaNoite())
+console.log('Modulo B', moduloB)
