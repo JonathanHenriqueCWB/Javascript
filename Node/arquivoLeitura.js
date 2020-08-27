3d4cc1ae0bb77233ee25e0f6f97750f8173f456c
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-08-27 20:24:21 -0300

    Node - lendo arquivos

---
+++ Node/arquivoLeitura.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+
+const caminho = __dirname + '/arquivo.json'
+
+// sincrono
+const conteudo = fs.readFileSync(caminho, 'utf-8')
+console.log(conteudo)
+
+// assincrono
+fs.readFile(caminho, 'utf-8', (err, conteudo) => {
+    const config = JSON.parse(conteudo)
+    console.log(`${config.db.host}:${config.db.port}`)
+})
+
+// Outra maneira de ler um arquivo JSON, dessa maneira ele ja converte o JSON (string) em objeto
+const config = require('./arquivo.json')
+console.log(config.db)
+
+// Le os arquivos da pasta
+fs.readdir(__dirname, (err, arquivos) =>{
+    console.log('Conteudo da pasta')
+    console.log(arquivos)
+})
