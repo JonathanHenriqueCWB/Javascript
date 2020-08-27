d255e82b1a2c6abb5ca21de4b90c31db7385e1d6
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-08-27 17:31:42 -0300

    Node - passando parametros entre modulos

---
+++ Node/passandoParametrosCliente.js
@@ -0,0 +1,8 @@
+const parametros = require('./passandoParametros')('Mario', 'Yosh', 'Luigi')
+
+/**
+    Dessa maneira dará erro caso os parametros forem passado no require
+    console.log(parametros('Jose', 'Marcos'))
+ */
+
+ console.log(parametros)
