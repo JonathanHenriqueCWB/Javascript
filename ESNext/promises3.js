5198493feb111542eec060fa2f931a705d59054a
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-09-15 20:28:38 -0300

    ESNext - ex promise3

---
+++ ESNext/promises3.js
@@ -0,0 +1,10 @@
+function gerarNumeros(min, max){
+    if(min > max) [max, min] = [min, max]
+    return new Promise(function(resolve){
+        const fator = max - min + 1
+        const aleatorio = parseInt(Math.random() * fator) + min
+        resolve(aleatorio)
+    })
+}
+
+gerarNumeros(1, 10).then(num => num*100).then( numX10 => `O numero foi multiplicado por 100: ${numX10}`).then(console.log)
