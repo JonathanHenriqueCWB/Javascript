6c28dbca13fb18ec4bd5cb16be38476f2edd4f9d
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-09-17 20:18:48 -0300

    ESNext - async await

---
+++ ESNext/asyncAwait1.js
@@ -0,0 +1,28 @@
+const { resolve } = require("path")
+
+function imprimir(tempo = 2000){
+    return new Promise(resolve => {
+        setTimeout(() => resolve('Função executada com sucesso!'), tempo)
+    })
+}
+function label (){
+    return new Promise(resolve => {
+        resolve(10)
+    })
+}
+
+// imprimir(2000).then(console.log).then(imprimir).then(console.log).then(imprimir).then(console.log)
+
+async function mostrar(){
+    const valor = await label()
+
+    await imprimir(1500)
+    console.log(`Async/Await 1... ${valor}`)
+
+    await imprimir(1500)
+    console.log(`Async/Await 1... ${valor + 1}`)
+
+    await imprimir(1500)
+    console.log(`Async/Await 1... ${valor + 2}`)
+}
+mostrar()
