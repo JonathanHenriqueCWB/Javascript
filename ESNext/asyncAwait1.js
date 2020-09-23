3f9207fc66c28e236856e45ed0d7956c3f2b1963
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-09-23 17:58:02 -0300

    HTML - anatomia da pagina html

--- ESNext/asyncAwait1.js
+++ ESNext/asyncAwait1.js
@@ -1,5 +1,3 @@
-const { resolve } = require("path")
-
 function imprimir(tempo = 2000){
     return new Promise(resolve => {
         setTimeout(() => resolve('Função executada com sucesso!'), tempo)
